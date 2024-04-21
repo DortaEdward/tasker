1885647fbda48e02337b7ad14ea53cc672c62254
dortaedward <dortaedward@gmail.com>
2024-04-21 17:06:49 -0400

    renamed several items

--- src/app/(dashboard)/_components/DashboardContainer.tsx
+++ src/app/(dashboard)/_components/DashboardContainer.tsx
@@ -1,20 +1,21 @@
 "use client"
 import { useState } from "react"
 import Sidebar from "./Sidebar"
-import CreateBoardModal from "./CreateBoardModal"
+import CreateListModal from "./CreateListModal"
 
 export default function DashboardContainer({ children }: { children: React.ReactNode }) {
+  const [toggleCreateList, setToggleCreateList] = useState<boolean>(false);
   const [toggleCreateBoard, setToggleCreateBoard] = useState<boolean>(false);
   return (
     <div className="min-w-screen min-h-screen bg-slate-800 flex relative">
-      <Sidebar setToggleCreateBoard={setToggleCreateBoard} />
+      <Sidebar setToggleCreateBoard={setToggleCreateList} />
       <div className="flex-1 flex flex-col">
         <header className="w-full h-20 border">Outline</header>
         <div className="flex-1 border">
           {children}
         </div>
       </div>
-      <CreateBoardModal toggleCreateBoard={toggleCreateBoard} setToggleCreateBoard={setToggleCreateBoard} />
+      <CreateListModal toggleCreateList={toggleCreateList} setToggleCreateList={setToggleCreateList} />
     </div>
   )
 }
